d713b1ef60fa72fc9abec918dab16100f96b0ee0
merakissb <mbustosquiroz@gmail.com>
2023-09-20 00:30:15 -0300

    añade font-awesome

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
 const { Pool } = require('pg');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
-});
+// const multer = require('multer');
 
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.listen(3001, () => console.log('Server running on port 3001'));
-
-// conexion a la base de datos
 
 const pool = new Pool({
   user: 'postgres',
@@ -36,27 +35,16 @@ pool.connect((err) => {
   console.log('Connected to database');
 });
 
-//rutas
-app.post('/posts', upload.single('img'), async (req, res) => {
+app.post('/posts', async (req, res) => {
   try {
-
-    //validacion de datos
-    if (!req.file) {
-      return res.status(400).json({ msg: 'Archivo de imagen requerido' });
-    }
-
-    // Luego valida otros datos
-    if (!req.body.titulo || !req.body.descripcion) {
+    const { titulo, url, descripcion } = req.body;
+    if (!titulo || !url || !descripcion) {
       return res.status(400).json({ msg: 'Datos incompletos' });
     }
-
-    const { titulo, descripcion } = req.body;
-    const img = req.file.filename;
     const dbInstance = await pool.connect();
     const query = `INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3)`;
-    const values = [titulo, img, descripcion];
+    const values = [titulo, url, descripcion];
     await dbInstance.query(query, values);
-
     res.status(200).json({ msg: 'Post agregado' });
     dbInstance.release();
   } catch (error) {
@@ -70,7 +58,6 @@ app.get('/posts', async (req, res) => {
     const dbInstance = await pool.connect();
     const query = `SELECT * FROM posts`;
     const response = await dbInstance.query(query);
-
     res.status(200).json(response.rows);
     dbInstance.release();
   } catch (error) {
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ function Post({
   return (
     <div className="card col-12 col-sm-4 d-inline mx-0 px-3">
       <div className="card-body  p-0">
-        <img className="card-img-top " src={`uploads/${img}`} />
+        <img className="card-img-top " src={img} />
         <div className="p-3">
           <h4 className="card-title">{titulo}</h4>
           <p className="card-text">{descripcion}</p>
